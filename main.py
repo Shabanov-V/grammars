391ffc20f787cf5149e44262f66a567041beed05
Vladimir <koprus77@gmail.com>
2017-12-30 00:48:40 +0300

    second algo OK

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ def solver(graph_file_path, grammar_file_path):
     N = max(list(map(int, re.findall('\d+', graph)))) + 1
     arr = [[''] * N for i in range(N)]
     paths = re.findall('(\d+) -> (\d+)\[label=\"(.+)\"\]', graph)
-    mp1 = re.findall('(.+) -> (.+)', grammar)
-    #print (any((c not in 'CSZSZSZSZSZSZSZSZSZ') for c in ['S', 'Z', 'y']))
     for i in paths:
         (a, b, c) = i
         arr[int(a)][int(b)] = ''.join(re.findall(r"(.+) -> %s" % c, grammar))
@@ -38,3 +36,57 @@ def solver(graph_file_path, grammar_file_path):
     grammar_file.close()
     graph_file.close()
     return ans
+
+
+
+def algo2(graph_file_path, grammar_file_path):
+    graph_file = open(graph_file_path, 'r')
+    grammar_file = open(grammar_file_path, 'r')
+    graph = graph_file.read()
+    grammar = grammar_file.read()
+    N = max(list(map(int, re.findall('\d+', graph)))) + 1
+    arr = [[''] * N for i in range(N)]
+    visited = [False] * N
+    paths = re.findall('(\d+) -> (\d+)\[label=\"(.+)\"\]', graph)
+    grammar_rules = list(zip(*(re.findall(r"(.+) -> (.+)", grammar))))
+    grammar_rules[1] = [x.replace(" ", "") for x in grammar_rules[1]]
+    M = len(grammar_rules[1])
+    max_depth = max([len(x) for x in grammar_rules[1]])
+    for i in paths:
+        (a, b, c) = i
+        arr[int(a)][int(b)] = c
+    f = [True]
+
+    def dfs(start, cur, cur_depth, path):
+        visited[cur] = True
+        t = [i for i, x in enumerate(grammar_rules[1]) if x == path]
+        for j in t:
+            if not (grammar_rules[0][j] in arr[start][cur]):
+                f[0] = True
+                arr[start][cur] += grammar_rules[0][j]
+        if (cur_depth == max_depth):
+            return
+        for i in range(N):
+            if (arr[cur][i] != ''):
+                for j in arr[cur][i]:
+                    dfs(start, i, cur_depth + 1, path + j)
+
+    while (f[0]):
+        f[0] = False
+        for i in range(N):
+            dfs(i, i, 0, '')
+
+     # dfs(24, 24, -1, '')
+
+
+    ans = 0
+    for i in range(N):
+        for j in range(N):
+            if ('S' in arr[i][j]):
+                ans += 1
+    grammar_file.close()
+    graph_file.close()
+    return ans
+
+if __name__ == '__main__':
+    print (algo2('data/skos.dot', 'data/grammar1'))
--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import unittest
 from main import solver
+from main import algo2
 
-class TestStringMethods(unittest.TestCase):
+class TestFirstAlgo(unittest.TestCase):
     def test_1_1(self):
         self.assertEqual(solver('data/skos.dot', 'data/grammar1CNF'), 810)
 
@@ -72,6 +73,77 @@ class TestStringMethods(unittest.TestCase):
     def test_11_2(self):
         self.assertEqual(solver('data/pizza.dot', 'data/grammar2CNF'), 1262)
 
+class TestSecondAlgo(unittest.TestCase):
+    def test_1_1(self):
+        self.assertEqual(algo2('data/skos.dot', 'data/grammar1CNF'), 810)
+
+    def test_2_1(self):
+        self.assertEqual(algo2('data/generations.dot', 'data/grammar1CNF'), 2164)
+
+    def test_3_1(self):
+        self.assertEqual(algo2('data/travel.dot', 'data/grammar1CNF'), 2499)
+
+    def atest_4_1(self):
+        self.assertEqual(algo2('data/univ-bench.dot', 'data/grammar1CNF'), 2540)
+
+    def test_5_1(self):
+        self.assertEqual(algo2('data/atom-primitive.dot', 'data/grammar1CNF'), 15454)
+
+    def test_6_1(self):
+        self.assertEqual(algo2('data/biomedical-mesure-primitive.dot', 'data/grammar1CNF'), 15156)
+
+    def test_7_1(self):
+        self.assertEqual(algo2('data/foaf.dot', 'data/grammar1CNF'), 4118)
+
+    def test_8_1(self):
+        self.assertEqual(algo2('data/people_pets.dot', 'data/grammar1CNF'), 9472)
+
+    def test_9_1(self):
+        self.assertEqual(algo2('data/funding.dot', 'data/grammar1CNF'), 17634)
+
+    def test_10_1(self):
+        self.assertEqual(algo2('data/wine.dot', 'data/grammar1CNF'), 66572)
+
+    def test_11_1(self):
+        self.assertEqual(algo2('data/pizza.dot', 'data/grammar1CNF'), 56195)
+    #
+    # #     sad
+    # # sad
+    # #     sad
+    #
+    def test_1_2(self):
+        self.assertEqual(algo2('data/skos.dot', 'data/grammar2CNF'), 1)
+
+    def test_2_2(self):
+        self.assertEqual(algo2('data/generations.dot', 'data/grammar2CNF'), 0)
+
+    def test_3_2(self):
+        self.assertEqual(algo2('data/travel.dot', 'data/grammar2CNF'), 63)
+
+    def test_4_2(self):
+        self.assertEqual(algo2('data/univ-bench.dot', 'data/grammar2CNF'), 81)
+
+    def test_5_2(self):
+        self.assertEqual(algo2('data/atom-primitive.dot', 'data/grammar2CNF'), 122)
+
+    def test_6_2(self):
+        self.assertEqual(algo2('data/biomedical-mesure-primitive.dot', 'data/grammar2CNF'), 2871)
+
+    def test_7_2(self):
+        self.assertEqual(algo2('data/foaf.dot', 'data/grammar2CNF'), 10)
+
+    def test_8_2(self):
+        self.assertEqual(algo2('data/people_pets.dot', 'data/grammar2CNF'), 37)
+
+    def test_9_2(self):
+        self.assertEqual(algo2('data/funding.dot', 'data/grammar2CNF'), 1158)
+
+    def test_10_2(self):
+        self.assertEqual(algo2('data/wine.dot', 'data/grammar2CNF'), 133)
+
+    def test_11_2(self):
+        self.assertEqual(algo2('data/pizza.dot', 'data/grammar2CNF'), 1262)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(sortTestMethodsUsing = None)
