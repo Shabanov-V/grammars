a04564167447f4a5c29eced950962730b9c251ee
Vladimir <koprus77@gmail.com>
2018-01-23 02:20:08 +0300

    add graph grammatic input

--- main.py
+++ main.py
@@ -25,20 +25,20 @@ def algo1(graph_file_path, grammar_file_path):
                                 fnd = re.findall(r"(.+) -> %s\n" % t, grammar)
                                 if (any((c not in arr[i][j]) for c in fnd)):
                                     f = True
-                                    arr[i][j] += ''.join(fnd)
+                                    arr[i][j] += ',' + ''.join(fnd) + ','
                                     #print(fnd, arr[i][j])
     ans = []
     for i in range(N):
         for j in range(N):
             if ('S' in arr[i][j]):
-                ans.append(arr[i][j])
+                ans.append(str(i) + ',S,' + str(j))
     grammar_file.close()
     graph_file.close()
     return ans
 
 
 
-def algo2(graph_file_path, grammar_file_path):
+def algo22(graph_file_path, grammar_file_path):
     graph_file = open(graph_file_path, 'r')
     grammar_file = open(grammar_file_path, 'r')
     graph = graph_file.read()
@@ -79,7 +79,7 @@ def algo2(graph_file_path, grammar_file_path):
     for i in range(N):
         for j in range(N):
             if ('S' in arr[i][j]):
-                ans.append(str(i) + 'S' + str(j))
+                ans.append(str(i) + ',S,' + str(j))
     grammar_file.close()
     graph_file.close()
     return ans
@@ -88,12 +88,13 @@ def algo2(graph_file_path, grammar_file_path):
 def graph_to_array(graph_file_path):
     graph_file = open(graph_file_path, 'r')
     graph = graph_file.read()
-    N = max(list(map(int, re.findall('\d+', graph)))) + 1
-    arr = [[''] * N for i in range(N)]
     paths = re.findall('(\d+) -> (\d+)\[label=\"(.+)\"\]', graph)
+    N = len(set([i[0] for i in paths] + [i[1] for i in paths]))
+    arr = [[''] * N for i in range(N)]
     for i in paths:
         (a, b, c) = i
         arr[int(a)][int(b)] = c
+    graph_file.close()
     return arr
 
 
@@ -115,11 +116,11 @@ def grammar_to_graph(grammar_file_path):
         order += [(i, net)]
         cur_grammars = list(filter(lambda x: x[0] == i, list(zip(*grammar_rules))))
         counter = 1
-        #print("cur_grammars = " + str(cur_grammars))
-        rls = [s.split(' ') for s in list(zip(*cur_grammars))[1]]
-        #print ("rls = " + str(rls))
-        D = sum(list(map(len, rls))) - len(rls) + 3
-        #print("D = " + str(D))
+        # print("cur_grammars = " + str(cur_grammars))
+        rls = [s for s in list(zip(*cur_grammars))[1]]
+        # print ("rls = " + str(rls))
+        D = sum(list(map(len, rls))) - len(rls) + 2
+        # print("D = " + str(D))
         starts += [str(net)]
         for j in cur_grammars:
             t, rules = j # rules = 'F F'
@@ -148,7 +149,28 @@ def grammar_to_graph(grammar_file_path):
     for i in range(max_cur + 1):
         for j in range(max_cur + 1):
             ans[i][j] = arr[i][j]
-    return ans, starts, ends, order
+    grammar_file.close()
+    return ans, starts, [ends, []], order
+
+
+def in_grammar(file_name):
+    grammar_file = open(file_name, 'r')
+    grammar = grammar_file.read()
+    grammar_rules = (re.findall(r'(\d+) -> (\d+)\[label.*"(.+)"\]', grammar))
+    # print(grammar_rules)
+    grammar_rules = [(int(i[0]), int(i[1]), i[2]) for i in grammar_rules]
+    mx = max([i[0] for i in grammar_rules] + [i[1] for i in grammar_rules])
+    arr = arr = [[''] * (mx + 1) for i in range(mx + 1)]
+    for i in grammar_rules:
+        arr[i[0]][i[1]] = i[2]
+    starts = (re.findall(r'(\d+)\[label=".{1,3}",.*color="green".*\]', grammar))
+    ends = (re.findall(r'(\d+)\[label=".{1,3}",.*shape="doublecircle".*\]', grammar))
+    order_ends = (re.findall(r'(\d+)\[label="(.{1,3})",.*shape="doublecircle".*\]', grammar))
+    ends = [ends, order_ends]
+    order = (re.findall(r'(\d+)\[label="(.{1,3})",.*color="green".*\]', grammar))
+    order = [(i[1], int(i[0])) for i in order]
+    grammar_file.close()
+    return (arr, starts, ends, order)
 
 
 def out_graph(arr, starts = [], ends = [], comments=''):
@@ -184,15 +206,79 @@ rankdir=LR
     tmp = []
     for i in arr:
         a, b, c = i
-        t += '"' + ','.join(str(x) for x in a) +  '" -> "' + ','.join(str(x) for x in c) + '"' + '[label="' + str(b) + '"]\n'
+        t += '"' + ','.join(str(x) for x in a) + '" -> "' + ','.join(str(x) for x in c) + '"' + '[label="' + str(b) + '"]\n'
     ans += t
     ans += '}'
     return ans
 
+def algo2(graph_file_path, grammar_file_path, is_test=False):
+    if is_test:
+        return algo22(graph_file_path, grammar_file_path)
+    ans = []
+    graph_file = open(graph_file_path, 'r')
+    grammar_file = open(grammar_file_path, 'r')
+    graph = graph_file.read()
+    grammar = grammar_file.read()
+    starts = (re.findall(r'(\d+)\[label="(.{1,3})",.*color="green".*\]', grammar))
+    starts = [(t[1], t[0]) for t in starts]
+    ends = (re.findall(r'(\d+)\[label="(.{1,3})",.*shape="doublecircle".*\]', grammar))
+    ends = [(t[1], t[0]) for t in ends]
+    N = max(list(map(int, re.findall('\d+', graph)))) + 1
+    arr_graph = [[''] * N for i in range(N)]
+    check = algo3(graph_file_path, grammar_file_path, is_test)
+    ans = check
+    paths = re.findall('(\d+) -> (\d+)\[label=\"(.+)\"\]', graph)
+    for i in paths:
+        (a, b, c) = i
+        arr_graph[int(a)][int(b)] = c
+    M = max(list(map(int, re.findall('\d+', grammar)))) + 1
+    arr_grammar = [[''] * N for i in range(M)]
+    paths = re.findall('(\d+) -> (\d+)\[label=\"(.+)\"\]', grammar)
+    for i in paths:
+        (a, b, c) = i
+        arr_grammar[int(a)][int(b)] = c
 
-def algo3(graph_file_path, grammar_file_path):
+    f =[True]
+    p = [0]
+    z = [0]
+    def dfs(start, cur, path):
+        t = [i for i, x in enumerate(arr_grammar[1]) if x == path]
+        for j in range(len(t)):
+            if not (arr_grammar[0][j] in arr_graph[start]):
+                f[0] = True
+                arr_graph[start][j] += arr_grammar[0][j]
+        if (cur == arr_graph[p[0]][z[0]]):
+            return
+        z[0] += int(arr_grammar[0][j])
+        for i in range(N):
+            p[0] += 1
+            if (arr_graph[cur][i] != ''):
+                for j in arr_graph[cur][i]:
+                    dfs(start, i, path + j)
+
+    if f[0]:
+        f[0] = False
+        for i in range(len(arr_graph)):
+            for j in range(len(starts)):
+                p[0] = i
+                z[0] = j
+                dfs(i, arr_graph[i][j], '')
+    return ans
+
+
+def algo3(graph_file_path, grammar_file_path, is_test=False):
     arr = graph_to_array(graph_file_path)
-    grammar, starts, ends, order = grammar_to_graph(grammar_file_path)
+    # print(arr)
+    if is_test:
+        grammar, starts, ends, order = grammar_to_graph(grammar_file_path)
+    else:
+        grammar, starts, ends, order = in_grammar(grammar_file_path)
+    order_ends = dict(ends[1])
+    # print(order_ends)
+    ends = ends[0]
+    # print(grammar)  # [['', 'A', 'a', '', '', ''], ['', '', 'a', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', 'A', ''], ['', '', '', '', '', 'b'], ['', '', '', '', '', '']]
+    #ends += starts
+    # print(starts, ends, order)  # ['0'] ['3', '0'] [('S', 0)]
     ans = []
     starts_dic = dict(order)
     conf_set = set()
@@ -202,21 +288,20 @@ def algo3(graph_file_path, grammar_file_path):
         conf_set = set()
         colored = set()
         while conf:
-            if (conf[0] in conf_set):
-                enter_pos, gramm_pos, gss_node = conf.pop(0)
-                # print("--cur_node = ", end='')
-                # print(enter_pos, gramm_pos, gss_node)
-                continue
-            conf_set.add(conf[0])
-            enter_pos, gramm_pos, gss_node = conf.pop(0)
-            if str(gramm_pos) in ends:
-                ans += [str(gss_node[0]) + str(gss_node[1]) + str(enter_pos)]
+            enter_pos, gramm_pos, gss_node = conf[0]
+            # print("cur_node = ", end='')
+            # print(enter_pos, gramm_pos, gss_node)
+            if str(gramm_pos) in ends and (is_test == True or is_test == False and order_ends[str(gramm_pos)] == gss_node[1]):
+                ans += [str(gss_node[0]) + ',' + str(gss_node[1]) + ',' + str(enter_pos)]
                 for i in gss:
                     fr, ed, to = i
                     if fr == gss_node:
                         conf += [(enter_pos, ed, to)]
-            # print("cur_node = ", end='')
-            # print(enter_pos, gramm_pos, gss_node)
+            if (conf[0] in conf_set):
+                conf.pop(0)
+                continue
+            conf_set.add(conf[0])
+            conf.pop(0)
             for j in range(len(arr[enter_pos])):
                 if arr[enter_pos][j] != '':
                     # print(arr[enter_pos][j])
@@ -226,21 +311,26 @@ def algo3(graph_file_path, grammar_file_path):
                         if grammar_symb == next_symb:
                             conf += [(j, i, gss_node)]
                         if re.match(r'[A-Z]+\d*', grammar_symb):
+                            # print("GSS CONF + : ", end='')
+                            # print((enter_pos, starts_dic[grammar_symb], (enter_pos, grammar_symb)))
                             conf += [(enter_pos, starts_dic[grammar_symb], (enter_pos, grammar_symb))]
                             # print("GSS: ", end='')
                             # print(((enter_pos, grammar_symb), i, gss_node))
                             gss.append(((enter_pos, grammar_symb), i, gss_node))
 
     # f = open('graph.dot', 'w')
-    # for i in out_graph(grammar, starts, ends, str(order)):
+    # for i in out_graph_list(gss):
     #     f.write(i)
     # f.close()
     # os.system('dot -Tpdf graph.dot -o testGraph.pdf')
 
     res = []
     for i in ans:
-        if ('S' in i):
+        if (',S,' in i):
             res.append(i)
 
     res = set(res)
-    return res
+    if (is_test):
+        return res
+    else:
+        return '\n'.join(res)
--- tests.py
+++ tests.py
@@ -81,46 +81,46 @@ class TestFirstAlgo(unittest.TestCase):
 class TestSecondAlgo(unittest.TestCase):
 
     def test0001_1(self):
-        self.assertEqual(len(algo2('data/simple1.dot', 'data/simple1')), 15)
+        self.assertEqual(len(algo2('data/simple1.dot', 'data/simple1', True)), 15)
 
     def test001_1(self):
-        self.assertEqual(len(algo2('data/simple2.dot', 'data/simple2')), 12)
+        self.assertEqual(len(algo2('data/simple2.dot', 'data/simple2', True)), 12)
 
     def test01_1(self):
-        self.assertEqual(len(algo2('data/simple4.dot', 'data/simple4')), 1)
+        self.assertEqual(len(algo2('data/simple4.dot', 'data/simple4')), 17)
 
     def test1_1(self):
-        self.assertEqual(len(algo2('data/skos.dot', 'data/grammar1')), 810)
+        self.assertEqual(len(algo2('data/skos.dot', 'data/grammar1', True)), 810)
 
     def test2_1(self):
-        self.assertEqual(len(algo2('data/generations.dot', 'data/grammar1')), 2164)
+        self.assertEqual(len(algo2('data/generations.dot', 'data/grammar1', True)), 2164)
 
     def test3_1(self):
-        self.assertEqual(len(algo2('data/travel.dot', 'data/grammar1')), 2499)
+        self.assertEqual(len(algo2('data/travel.dot', 'data/grammar1', True)), 2499)
 
     def atest4_1(self):
-        self.assertEqual(len(algo2('data/univ-bench.dot', 'data/grammar1')), 2540)
+        self.assertEqual(len(algo2('data/univ-bench.dot', 'data/grammar1', True)), 2540)
 
     def test9945_1(self):
-        self.assertEqual(len(algo2('data/atom-primitive.dot', 'data/grammar1')), 15454)
+        self.assertEqual(len(algo2('data/atom-primitive.dot', 'data/grammar1', True)), 15454)
 
     def test9936_1(self):
-        self.assertEqual(len(algo2('data/biomedical-mesure-primitive.dot', 'data/grammar1')), 15156)
+        self.assertEqual(len(algo2('data/biomedical-mesure-primitive.dot', 'data/grammar1', True)), 15156)
 
     def test7_1(self):
-        self.assertEqual(len(algo2('data/foaf.dot', 'data/grammar1')), 4118)
+        self.assertEqual(len(algo2('data/foaf.dot', 'data/grammar1', True)), 4118)
 
     def test9918_1(self):
-        self.assertEqual(len(algo2('data/people_pets.dot', 'data/grammar1')), 9472)
+        self.assertEqual(len(algo2('data/people_pets.dot', 'data/grammar1', True)), 9472)
 
     def test9979_1(self):
-        self.assertEqual(len(algo2('data/funding.dot', 'data/grammar1')), 17634)
+        self.assertEqual(len(algo2('data/funding.dot', 'data/grammar1', True)), 17634)
 
     def test99910_1(self):
-        self.assertEqual(len(algo2('data/wine.dot', 'data/grammar1')), 66572)
+        self.assertEqual(len(algo2('data/wine.dot', 'data/grammar1', True)), 66572)
 
     def test99811_1(self):
-        self.assertEqual(len(algo2('data/pizza.dot', 'data/grammar1')), 56195)
+        self.assertEqual(len(algo2('data/pizza.dot', 'data/grammar1', True)), 56195)
 
     #
     # #     sad
@@ -129,82 +129,82 @@ class TestSecondAlgo(unittest.TestCase):
     #
 
     def test1_2(self):
-        self.assertEqual(len(algo2('data/skos.dot', 'data/grammar2')), 1)
+        self.assertEqual(len(algo2('data/skos.dot', 'data/grammar2', True)), 1)
 
     def test2_2(self):
-        self.assertEqual(len(algo2('data/generations.dot', 'data/grammar2')), 0)
+        self.assertEqual(len(algo2('data/generations.dot', 'data/grammar2', True)), 0)
 
     def test3_2(self):
-        self.assertEqual(len(algo2('data/travel.dot', 'data/grammar2')), 63)
+        self.assertEqual(len(algo2('data/travel.dot', 'data/grammar2', True)), 63)
 
     def test4_2(self):
-        self.assertEqual(len(algo2('data/univ-bench.dot', 'data/grammar2')), 81)
+        self.assertEqual(len(algo2('data/univ-bench.dot', 'data/grammar2', True)), 81)
 
     def test995_2(self):
-        self.assertEqual(len(algo2('data/atom-primitive.dot', 'data/grammar2')), 122)
+        self.assertEqual(len(algo2('data/atom-primitive.dot', 'data/grammar2', True)), 122)
 
     def test996_2(self):
-        self.assertEqual(len(algo2('data/biomedical-mesure-primitive.dot', 'data/grammar2')), 2871)
+        self.assertEqual(len(algo2('data/biomedical-mesure-primitive.dot', 'data/grammar2', True)), 2871)
 
     def test7_2(self):
-        self.assertEqual(len(algo2('data/foaf.dot', 'data/grammar2')), 10)
+        self.assertEqual(len(algo2('data/foaf.dot', 'data/grammar2', True)), 10)
 
     def test998_2(self):
-        self.assertEqual(len(algo2('data/people_pets.dot', 'data/grammar2')), 37)
+        self.assertEqual(len(algo2('data/people_pets.dot', 'data/grammar2', True)), 37)
 
     def test999_2(self):
-        self.assertEqual(len(algo2('data/funding.dot', 'data/grammar2')), 1158)
+        self.assertEqual(len(algo2('data/funding.dot', 'data/grammar2', True)), 1158)
 
     def test9910_2(self):
-        self.assertEqual(len(algo2('data/wine.dot', 'data/grammar2')), 133)
+        self.assertEqual(len(algo2('data/wine.dot', 'data/grammar2', True)), 133)
 
     def test9911_2(self):
-        self.assertEqual(len(algo2('data/pizza.dot', 'data/grammar2')), 1262)
+        self.assertEqual(len(algo2('data/pizza.dot', 'data/grammar2', True)), 1262)
 
 
 class TestThirdAlgo(unittest.TestCase):
 
     def test0001_1(self):
-        self.assertEqual(len(algo3('data/simple1.dot', 'data/simple1')), 15)
+        self.assertEqual(len(algo3('data/simple1.dot', 'data/simple1', True)), 15)
 
     def test001_1(self):
-        self.assertEqual(len(algo3('data/simple2.dot', 'data/simple2')), 12)
+        self.assertEqual(len(algo3('data/simple2.dot', 'data/simple2', True)), 12)
 
     def test01_1(self):
-        self.assertEqual(len(algo3('data/simple4.dot', 'data/simple4')), 1)
+        self.assertEqual(len(algo3('data/simple4.dot', 'data/simple4')), 17)
 
     def test1_1(self):
-        self.assertEqual(len(algo3('data/skos.dot', 'data/grammar1')), 810)
+        self.assertEqual(len(algo3('data/skos.dot', 'data/grammar1', True)), 810)
 
     def test2_1(self):
-        self.assertEqual(len(algo3('data/generations.dot', 'data/grammar1')), 2164)
+        self.assertEqual(len(algo3('data/generations.dot', 'data/grammar1', True)), 2164)
 
     def test3_1(self):
-        self.assertEqual(len(algo3('data/travel.dot', 'data/grammar1')), 2499)
+        self.assertEqual(len(algo3('data/travel.dot', 'data/grammar1', True)), 2499)
 
     def atest4_1(self):
-        self.assertEqual(len(algo3('data/univ-bench.dot', 'data/grammar1')), 2540)
+        self.assertEqual(len(algo3('data/univ-bench.dot', 'data/grammar1', True)), 2540)
 
     def test995_1(self):
-        self.assertEqual(len(algo3('data/atom-primitive.dot', 'data/grammar1')), 15454)
+        self.assertEqual(len(algo3('data/atom-primitive.dot', 'data/grammar1', True)), 15454)
 
     def test996_1(self):
-        self.assertEqual(len(algo3('data/biomedical-mesure-primitive.dot', 'data/grammar1')), 15156)
+        self.assertEqual(len(algo3('data/biomedical-mesure-primitive.dot', 'data/grammar1', True)), 15156)
 
     def test7_1(self):
-        self.assertEqual(len(algo3('data/foaf.dot', 'data/grammar1')), 4118)
+        self.assertEqual(len(algo3('data/foaf.dot', 'data/grammar1', True)), 4118)
 
     def test998_1(self):
-        self.assertEqual(len(algo3('data/people_pets.dot', 'data/grammar1')), 9472)
+        self.assertEqual(len(algo3('data/people_pets.dot', 'data/grammar1', True)), 9472)
 
     def test999_1(self):
-        self.assertEqual(len(algo3('data/funding.dot', 'data/grammar1')), 17634)
+        self.assertEqual(len(algo3('data/funding.dot', 'data/grammar1', True)), 17634)
 
     def test9910_1(self):
-        self.assertEqual(len(algo3('data/wine.dot', 'data/grammar1')), 66572)
+        self.assertEqual(len(algo3('data/wine.dot', 'data/grammar1', True)), 66572)
 
     def test9911_1(self):
-        self.assertEqual(len(algo3('data/pizza.dot', 'data/grammar1')), 56195)
+        self.assertEqual(len(algo3('data/pizza.dot', 'data/grammar1', True)), 56195)
 
     #
     # #     sad
@@ -213,37 +213,37 @@ class TestThirdAlgo(unittest.TestCase):
     #
 
     def test1_2(self):
-        self.assertEqual(len(algo3('data/skos.dot', 'data/grammar2')), 1)
+        self.assertEqual(len(algo3('data/skos.dot', 'data/grammar2', True)), 1)
 
     def test2_2(self):
-        self.assertEqual(len(algo3('data/generations.dot', 'data/grammar2')), 0)
+        self.assertEqual(len(algo3('data/generations.dot', 'data/grammar2', True)), 0)
 
     def test3_2(self):
-        self.assertEqual(len(algo3('data/travel.dot', 'data/grammar2')), 63)
+        self.assertEqual(len(algo3('data/travel.dot', 'data/grammar2', True)), 63)
 
     def test4_2(self):
-        self.assertEqual(len(algo3('data/univ-bench.dot', 'data/grammar2')), 81)
+        self.assertEqual(len(algo3('data/univ-bench.dot', 'data/grammar2', True)), 81)
 
     def test995_2(self):
-        self.assertEqual(len(algo3('data/atom-primitive.dot', 'data/grammar2')), 122)
+        self.assertEqual(len(algo3('data/atom-primitive.dot', 'data/grammar2', True)), 122)
 
     def test996_2(self):
-        self.assertEqual(len(algo3('data/biomedical-mesure-primitive.dot', 'data/grammar2')), 2871)
+        self.assertEqual(len(algo3('data/biomedical-mesure-primitive.dot', 'data/grammar2', True)), 2871)
 
     def test7_2(self):
-        self.assertEqual(len(algo3('data/foaf.dot', 'data/grammar2')), 10)
+        self.assertEqual(len(algo3('data/foaf.dot', 'data/grammar2', True)), 10)
 
     def test998_2(self):
-        self.assertEqual(len(algo3('data/people_pets.dot', 'data/grammar2')), 37)
+        self.assertEqual(len(algo3('data/people_pets.dot', 'data/grammar2', True)), 37)
 
     def test999_2(self):
-        self.assertEqual(len(algo3('data/funding.dot', 'data/grammar2')), 1158)
+        self.assertEqual(len(algo3('data/funding.dot', 'data/grammar2', True)), 1158)
 
     def test9910_2(self):
-        self.assertEqual(len(algo3('data/wine.dot', 'data/grammar2')), 133)
+        self.assertEqual(len(algo3('data/wine.dot', 'data/grammar2', True)), 133)
 
     def test9911_2(self):
-        self.assertEqual(len(algo3('data/pizza.dot', 'data/grammar2')), 1262)
+        self.assertEqual(len(algo3('data/pizza.dot', 'data/grammar2', True)), 1262)
 
 
 if __name__ == '__main__':
